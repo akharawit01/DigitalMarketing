5529c1c93a0eae07a481807f0a85a54ee1151e45
Doi <tanasit@lannasoftworks.com>
2018-03-05 22:16:22 +0700

    update slide show button.

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,45 +5,62 @@ $(document).ready(function () {
         autoplay: true,
         autoplayTimeout: 2000,
         responsiveClass: true,
-        responsive:{
-            0:{
+        responsive: {
+            0: {
                 items: 2,
             },
-            600:{
+            600: {
                 items: 3,
             },
-            1000:{
+            1000: {
                 items: 5,
             }
         }
     });
 
-    $('a.nav-link').click(function(){
+    $('a.nav-link').click(function () {
         $('html, body').animate({
-            scrollTop: $( $(this).attr('href') ).offset().top - 80
+            scrollTop: $($(this).attr('href')).offset().top - 80
         }, 500);
         return false;
     });
+
+    //renderSlideshow();
 });
 
-(function() {
+(function () {
     'use strict';
-    window.addEventListener('load', function() {
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-      var forms = document.getElementsByClassName('needs-validation');
-      // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }else {
-            $('#modalValid').modal('show')
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add('was-validated');
-        }, false);
-      });
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                } else {
+                    $('#modalValid').modal('show')
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
     }, false);
-  })();
+})();
+
+var renderSlideshow = function () {
+
+    $("#slidesbeforeafter").html("");
+    for (var i = 1; i <= 31; i++) {
+        var filename = i < 10 ? "0" + i : "" + i;
+        filename += ".jpg";
+
+        var html = "<div><a href='assets/images/before-after/" + filename + "' data-lightbox='roadtrip'>";
+        html += "<img src='assets/images/before-after/" + filename + "' alt=''></a></div>";
+
+        $("#slidesbeforeafter").append(html);
+        console.log(filename);
+    }
+};
